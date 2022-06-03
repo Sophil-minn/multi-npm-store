16571f2ae5c373ab768ef25da6e1a4b290f1f4d7
minmin <1056640663@qq.com>
2022-06-03 23:57:40 +0800

    feat: 命令注册

--- beibei-test/bin/index.js
+++ beibei-test/bin/index.js
@@ -10,11 +10,23 @@ console.log(argv, 'argv---');
 
 const command = argv[2];
 
-if (lib[command]) {
-  lib[command]();
+const options = argv.slice(3);
+
+let [option, param] = options;
+
+option = option?.replace('--', '');
+
+if (command) {
+  if (lib[command]) {
+    lib[command]({ option, param });
+  } else {
+    console.log('无效的命令');
+  }
+} else {
+  console.log('请输入命令： ');
 }
 
 // console.log(lib, 'beibei-test-lib');
 
-console.log('猫咪贝贝0931');
-console.log('贝贝0931');
+// console.log('猫咪贝贝0931');
+// console.log('贝贝0931');
