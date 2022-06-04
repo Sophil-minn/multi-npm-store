b698bdd2fd76597e4db2e07f968e6a4ea1a93729
minmin <1056640663@qq.com>
2022-06-04 09:33:00 +0800

    feat: npm 发布 测试

--- beibei-test/bin/index.js
+++ beibei-test/bin/index.js
@@ -2,11 +2,11 @@
 
 const lib = require('beibei-test-lib');
 
-console.log(lib, 'lib');
+// console.log(lib, 'lib');
 
 // 注册一个命令 minn-test-init
 const argv = require('process').argv;
-console.log(argv, 'argv---');
+// console.log(argv, 'argv---');
 
 const command = argv[2];
 
@@ -20,10 +20,20 @@ if (command) {
   if (lib[command]) {
     lib[command]({ option, param });
   } else {
-    console.log('无效的命令');
+    console.log('20220604无效的命令');
   }
 } else {
-  console.log('请输入命令： ');
+  console.log('20220604请输入命令： ');
+}
+
+// 实现参数解析 --version 和 init --name
+
+if (command.startsWith('--') || command.startsWith('-')) {
+  const globalOption = command.replace(/--|-/g, '');
+  // console.log(globalOption, 'globalOption');
+  if (globalOption === "version" || globalOption === "V") {
+    console.log(globalOption, "20220604版本号： 1.3.0");
+  }
 }
 
 // console.log(lib, 'beibei-test-lib');
